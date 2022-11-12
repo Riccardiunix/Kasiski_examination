29cee972013c65a5e495f01bec824cd66337dbad
riccardiunix <riccardo.bordon.2014@gmail.com>
2022-11-12 23:44:57 +0100

    improvments

--- main.py
+++ main.py
@@ -5,20 +5,14 @@ import argparse
 
 def factorization(n):
     if not n in dp:
-        dp[n] = {}
-        dp[n][n] = 0
+        set_n = set()
+        set_n.add(n)
         for i in range(int(round(math.sqrt(n))), 1, -1):
             if n % i == 0:
-                dp[n][i] = 0
-                for j in factorization(i):
-                    dp[n][j] = 0
-                    dp[n][n//j] = 0
-                if i != n//i:
-                    dp[n][n//i] = 0
-                    for j in factorization(n//i):
-                        dp[n][j] = 0
-                        dp[n][n//j] = 0
-    return dp[n].keys()
+                set_n = set_n.union(factorization(i))
+                set_n = set_n.union(factorization(n//i))
+        dp[n] = set_n
+    return dp[n]
 
 start = time.time()
 
@@ -29,7 +23,7 @@ if __name__ == "__main__":
     parser.add_argument('-l', type=int, default=0, help="the letter frequency to use for the frequency analysis.\n0: italian; 1: english")
     args = parser.parse_args()
     
-    text = open(args.i, "r").read().lower().replace('\n', '')
+    text = open(args.i, 'r').read().lower().replace('\n', '')
     text_ok = ""
     allComb = {}
     comb = {}
@@ -44,53 +38,56 @@ if __name__ == "__main__":
     a = {}
     m = 0
     for i in range(dim-1):
-        pat = text_ok[i]
-        for j in range(i+1, min(dim, (dim//2)+i+1)):
-            pat += text_ok[j]
-            if pat in comb:
-                for l in comb[pat]:
+        ii = i+1
+        for j in range(ii, min(dim, (dim>>1)+ii)):
+            hpat = hash(text_ok[i:j+1])
+            if hpat in comb:
+                for l in comb[hpat]:
                     diff = i - l
-                    if diff in a:
-                        a[diff] += 1
-                    else:
-                        a[diff] = 1
-                m = max(m, i - comb[pat][0])
-                comb[pat].append(i)
-            elif pat in allComb:
-                comb[pat] = [allComb[pat], i]
-                del allComb[pat]
+                    a.setdefault(diff, 0)
+                    a[diff] += 1
+                comb[hpat].append(i)
+            elif hpat in allComb:
+                comb[hpat] = [allComb[hpat], i]
+                del allComb[hpat]
             else:
-                allComb[pat] = i
+                allComb[hpat] = i
     del comb
     del allComb
     
     print("Calculating key length... ")
     dp = {}
-    b = [0] * (m + 1)
-    for i in a.keys():
+    b = {}
+    for i in list(a.keys()):
         for j in factorization(i):
+            b.setdefault(j, 0)
             b[j] -= a[i]
+        del a[i]
     del dp
     
     fact = []
-    if m <= 1019:
-        fact = numpy.argsort(b[2:])[:min(m, 10)]
-        for i in range(min(m, 10)):
-            fact[i] = fact[i] + 2  
-    else:
-        for i in range(10):
-            mi = b[2]
-            miI = 2
-            for j in range(3, m+1):
-                if mi > b[j]:
-                    mi = b[j]
-                    miI = j
-            b[miI] = 0
-            fact.append(miI)
-    
-    perc = [11.293, 0.911, 4.822, 3.724, 12.044, 1.115, 1.742, 1.465, 9.752, 0.002, 0.00001, 5.644, 2.544, 7.087, 9.624, 2.918, 0.795, 6.555, 5.506, 5.973, 3.581, 2.220, 0.001, 0.006, 0.001, 0.675]
+    min_i, min_v = 0, 0
+    for j in list(b.keys()):
+        if b[j] == -1:
+            del b[j]
+        elif min_v > b[j]:
+            min_v = b[j]
+            min_i = j
+    del b[min_i]
+    fact = [min_i]
+
+    for i in range(9):
+        min_i, min_v = 0, 0
+        for j in b.keys():
+            if min_v > b[j]:
+                min_v = b[j]
+                min_i = j
+        del b[min_i]
+        fact.append(min_i)
+
+    perc = [11.293,0.911,4.822,3.724,12.044,1.115,1.742,1.465,9.752,0.002,0.00001,5.644,2.544,7.087,9.624,2.918,0.795,6.555,5.506,5.973,3.581,2.220,0.001,0.006,0.001,0.675]
     if args.l == 1:
-        perc = [8.167, 1.492, 2.782, 4.253, 12.702, 2.228, 2.015, 6.094, 6.966, 0.153, 0.772, 4.025, 2.406, 6.749, 7.507, 1.929, 0.095, 5.987, 6.327, 9.056, 2.758, 0.978, 2.360, 0.15, 1.974, 0.074]
+        perc = [8.167,1.492,2.782,4.253,12.702,2.228,2.015,6.094,6.966,0.153,0.772,4.025,2.406,6.749,7.507,1.929,0.095,5.987,6.327,9.056,2.758,0.978,2.360,0.15,1.974,0.074]
     
     if args.o != '':
         out = open(args.o, 'w')
